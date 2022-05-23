99319e4a78070140cc3cb1ac285cfe3e5c1dfd94
Денис <denisegorenko1@gmail.com>
2022-05-23 14:59:26 +0300

    00 - 04 lessons

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import './App.css';
+import Accordion from "./Components/Accordion";
+import Rating from "./Components/Rating";
+
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div>
+            <PageTitle title={"This is App component"}/>
+            <PageTitle title={"My friends"}/>
+            <Accordion titleValue={"Menu"} collapsed={false}/>
+            <Accordion titleValue={"Sub Menu"} collapsed={false}/>
+            Article 1
+            <Rating value={0}/>
+            Article 2
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
+        </div>
+    );
 }
+
+
+function PageTitle(props:any) {
+    return (
+        <div>{props.title}</div>
+    );
+}
+
+
 
 export default App;
